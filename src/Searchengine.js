252deae3ce42eb2eeefcef0ff5992ab506e47749
leila seif <120723092+Leilaseif@users.noreply.github.com>
2023-02-12 16:28:20 +0330

    failed

--- src/Searchengine.js
+++ src/Searchengine.js
@@ -2,44 +2,67 @@ import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./Searchengine.css";
 import axios from "axios";
+
 export default function Searchengine (){
+    const [ready , setReady]=useState(false);
     let [city, setCity]= useState("");
-    let [temperature, setTemperature]=useState({});
+    let [weatherData, setweatherData]=useState({});
 
     function handleSubmit(event){
         event.preventDefault();
+       
     }
     function updateCity(event){
         setCity(event.target.value);
     }
     function showTemperature (response){
-        setTemperature({
+        setweatherData({
             temper:response.data.main.temp , 
             wind:response.data.wind.speed ,
-            humidity:response.data.main.humidity
+            humidity:response.data.main.humidity,
+            // description: response.data.weather[0].description ,
+            // iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            // date: new Date(response.data.dt)
         });
+        setReady(true);
     }
-    let apiurl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9777feb9b9683ea2533ba76e82f84952&units=metric`
-    axios.get(apiurl).then(showTemperature)
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5f472b7acba333cd8a035ea85a0d4d4c&units=metric`;
+        axios.get(apiUrl).then(showTemperature);
+       
+   
+        
+    if(ready){    
     return (
-        <div>
-        <h2>New york</h2>
+      <div>
         <form className="forming" onSubmit={handleSubmit}>
             <input className="search" type="search" onChange={updateCity}/>
             <input className="submit" type="submit" value="submit" />
         </form >
+        <h2>{city}</h2>
+        <span className="description"> {weatherData.description} %</span><br />
+       
         <div className="temperature">
-        <span className="number">{Math.round(temperature.temper)}</span>
+        <span className="number">{Math.round(weatherData.temper)}</span>
         <span className="unit">°c</span>
         </div>
         <div className="row">
-            <div className="col-3 icon">&#9728;&#65039;</div>
+            <div className="col-3 icon">
+               {/* <img  src={weatherData.iconUrl} alt="" /> */}
+            </div>
             <div className="col-9 moredata">
-                <span className="humidity"> humidity:{temperature.humidity} %</span><br />
-                <span className="wind">wind:{temperature.wind} km/h</span>
+                <span className="humidity"> humidity:{weatherData.humidity} %</span><br />
+                <span className="wind">wind:{weatherData.wind} km/h</span>
                 </div>
         </div>
 
-        </div>
-    )
+     </div>
+    )}
+    else{
+        let city ="New York"
+        let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=82796af55a1157613b4fa827494ee65a&units=metric`;
+        axios.get(apiUrl).then(showTemperature);
+       
+        return(<p>loading</p>)
+        ;
+    }
 }
