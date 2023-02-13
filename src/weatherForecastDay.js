00795591bfb55fc59d552ab3b16f2e856edaaf64
leila seif <120723092+Leilaseif@users.noreply.github.com>
2023-02-13 18:55:54 +0330

    setting forecast and real data (part 2 )

--- src/weatherForecastDay.js
+++ src/weatherForecastDay.js
@@ -1,8 +1,16 @@
 import React from "react";
  export default function WeatherForecastDay (props){
+    function day(){
+        let date= new Date(props.data.dt*1000);
+        let day = date.getDay();
+        let days=["Sun","Mon","Tuesday","Wed","Thu","Fri","Sat "];
+       
+        return days[day];
+    
+    }
     return(
         <div>
-        <div className="dayForecast">{props.data.dt}</div>
+        <div className="dayForecast">{day()}</div>
         <div className="iconForecast">{props.data.weather[0].icon}</div>
         <div className="temperatureForecast">
         <span className="temperatureForecast-max">{Math.round(props.data.temp.max)}°</span>
